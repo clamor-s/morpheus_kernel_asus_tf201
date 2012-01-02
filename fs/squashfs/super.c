6c347056e6cae85dffded1dad757d2f30d3d74ff
Phillip Lougher <phillip@squashfs.org.uk>
2012-01-02 17:47:14 +0000

    Squashfs: fix mount time sanity check for corrupted superblock
    A Squashfs filesystem containing nothing but an empty directory,
    although unusual and ultimately pointless, is still valid.
    
    The directory_table >= next_table sanity check rejects these
    filesystems as invalid because the directory_table is empty and
    equal to next_table.
    
    Signed-off-by: Phillip Lougher <phillip@squashfs.org.uk>

--- fs/squashfs/super.c
+++ fs/squashfs/super.c
@@ -297,7 +297,7 @@ handle_fragments:
 
 check_directory_table:
 	/* Sanity check directory_table */
-	if (msblk->directory_table >= next_table) {
+	if (msblk->directory_table > next_table) {
 		err = -EINVAL;
 		goto failed_mount;
 	}
