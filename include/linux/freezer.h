a7776b6c41115891e6e4c340395cde9f37b6f9f1
Svyatoslav <clamor95@gmail.com>
2017-08-06 13:12:09 +0300

    [CIFS] Update cifs version to 1.78+
    From https://github.com/showp1984/bricked-hammerhead

--- include/linux/freezer.h
+++ include/linux/freezer.h
@@ -138,6 +138,25 @@ static inline void set_freezable_with_signal(void)
  * wait_event_interruptible_timeout(), originally defined in <linux/wait.h>
  */
 
+#define wait_event_freezekillable(wq, condition)			\
+({									\
+	int __retval;							\
+	freezer_do_not_count();						\
+	__retval = wait_event_killable(wq, (condition));		\
+	freezer_count();						\
+	__retval;							\
+})
+
+/* DO NOT ADD ANY NEW CALLERS OF THIS FUNCTION */
+#define wait_event_freezekillable_unsafe(wq, condition)			\
+({									\
+	int __retval;							\
+	freezer_do_not_count();						\
+	__retval = wait_event_killable(wq, (condition));		\
+	freezer_count_unsafe();						\
+	__retval;							\
+})
+
 #define wait_event_freezable(wq, condition)				\
 ({									\
 	int __retval;							\
@@ -187,6 +206,12 @@ static inline void set_freezable_with_signal(void) {}
 
 #define wait_event_freezable_timeout(wq, condition, timeout)		\
 		wait_event_interruptible_timeout(wq, condition, timeout)
+
+#define wait_event_freezekillable(wq, condition)			\
+		wait_event_killable(wq, condition)
+
+#define wait_event_freezekillable_unsafe(wq, condition)			\
+		wait_event_killable(wq, condition)
 
 #endif /* !CONFIG_FREEZER */
 
